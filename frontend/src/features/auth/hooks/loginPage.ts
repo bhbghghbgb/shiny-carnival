a788546037b2aa3abce889a3804832c385d2d797
TanPhat15 <phatran2k3@gmail.com>
2025-10-28 21:14:39 +0700

    feat : add ui login

---
+++ frontend/src/features/auth/hooks/loginPage.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useNavigate } from "@tanstack/react-router";
+import { Form, message } from "antd";
+import { useCallback, useState } from "react";
+import { authApi } from "../api/authApi";
+import { useAuthActions } from "../store/authStore";
+import type { LoginRequest } from "../types/api";
+
+/**
+ * Hook xử lý logic trang đăng nhập
+ */
+export const useLoginPage = () => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const { setAuth } = useAuthActions();
+
+  
+  const handleSubmit = useCallback(async (values: LoginRequest) => {
+    setLoading(true);
+    try {
+      const res = await authApi.login(values);
+
+      if (!res.isError && res.data) {
+        // ✅ Gọi action Zustand (không tạo loop)
+        setAuth({
+          user: res.data.user,
+          token: res.data.token,
+        });
+
+        message.success("Đăng nhập thành công!");
+        navigate({ to: "/" }); // điều hướng về trang chủ
+      } else {
+        message.error(res.message || "Đăng nhập thất bại!");
+      }
+    } catch (error: any) {
+      message.error(error.message || "Có lỗi xảy ra khi đăng nhập");
+    } finally {
+      setLoading(false);
+    }
+  }, [setAuth, navigate]);
+
+  return {
+    form,
+    loading,
+    handleSubmit,
+  };
+};
--- frontend/src/features/auth/pages/LoginPage.tsx
+++ frontend/src/features/auth/pages/LoginPage.tsx
@@ -1,5 +1,29 @@
+import { Card } from "antd";
 import React from 'react';
+import LoginForm from "../components/LoginForm";
 
 export const LoginPage: React.FC = () => {
-  return <div>Login Page</div>;
+ 
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 p-6">
+      <Card className="w-full max-w-md rounded-2xl shadow-xl p-6">
+        <div className="text-center mb-6">
+          <div className="mx-auto w-14 h-14 flex items-center justify-center bg-linear-to-br from-indigo-500 to-purple-500 rounded-lg text-white font-bold text-2xl">
+            TL
+          </div>
+          <h1 className="text-2xl font-semibold mt-4">Chào mừng trở lại</h1>
+          <p className="text-gray-500 text-sm">Đăng nhập vào tài khoản của bạn</p>
+        </div>
+
+        <LoginForm />
+
+        <p className="text-center text-sm text-gray-500 mt-6">
+          Chưa có tài khoản?{" "}
+          <a href="/register" className="text-indigo-600 hover:underline">
+            Đăng ký
+          </a>
+        </p>
+      </Card>
+    </div>
+  );
 };
