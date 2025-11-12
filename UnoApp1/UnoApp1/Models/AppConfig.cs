bd854c57336a19d502d432b985ba9ce03c38c176
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-11-13 02:35:16 +0800

    setup 3

--- UnoApp1/UnoApp1/Models/AppConfig.cs
+++
@@ -1,6 +0,0 @@
-namespace UnoApp1.Models;
-
-public record AppConfig
-{
-    public string? Environment { get; init; }
-}
--- UnoApp1/UnoApp1/Models/Entity.cs
+++
@@ -1,3 +0,0 @@
-namespace UnoApp1.Models;
-
-public record Entity(string Name);
