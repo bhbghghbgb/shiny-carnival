d636b8135b702b5b601a167740f4b80466992d86
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-11-13 14:25:57 +0800

    test thu two way + ui trigger viewmodel

--- UnoApp2/UnoApp2/Presentation/Page1.xaml.cs
+++ UnoApp2/UnoApp2/Presentation/Page1.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+﻿// Page1.xaml.cs
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +9,24 @@ namespace UnoApp2.Presentation;
 /// </summary>
 public sealed partial class Page1 : Page
 {
+    private readonly Page1ViewModel _viewModel;
+
     public Page1()
     {
         this.InitializeComponent();
+
+        // Manual ViewModel Instantiation (for simple demo)
+        _viewModel = new Page1ViewModel();
+
+        // CRITICAL: Set the DataContext so XAML bindings work
+        this.DataContext = _viewModel;
+    }
+
+    // Optional: Override to manage ViewModel lifecycle if navigating
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+        base.OnNavigatedFrom(e);
+        // Clean up the background task when the page is closed/navigated away from
+        _viewModel.Dispose();
     }
 }
-
--- UnoApp2/UnoApp2/Presentation/Page1ViewModel.cs
+++ UnoApp2/UnoApp2/Presentation/Page1ViewModel.cs
@@ -1,6 +1,54 @@
-﻿namespace UnoApp2.Presentation;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
 
-public class Page1ViewModel
+namespace UnoApp2.Presentation;
+
+// Inherit from ObservableObject to enable property change notification
+public partial class Page1ViewModel : ObservableObject
 {
-    
+    private readonly CancellationTokenSource _cts = new();
+
+    // The counter property with automatic change notification
+    [ObservableProperty] private int _counter;
+
+    // The title property is also observable
+    [ObservableProperty] private string _title = "Uno MVVM Counter";
+
+    public Page1ViewModel()
+    {
+        // Start the background counter when the ViewModel is created
+        _ = StartAutoIncrementAsync(_cts.Token);
+    }
+
+    // Command to increase the counter by 10 (executed by the button)
+    // The name will be generated as 'IncreaseByTenCommand'
+    [RelayCommand]
+    private async Task IncreaseByTenAsync()
+    {
+        // Simulate a brief asynchronous operation (like saving the click count)
+        await Task.Delay(100);
+        Counter += 10;
+    }
+
+    // Private method to run the background task
+    private async Task StartAutoIncrementAsync(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            await Task.Delay(1000, token); // Wait for 1 second
+            if (!token.IsCancellationRequested)
+            {
+                // Note: The UI updates automatically because 'Counter' is an ObservableProperty
+                Counter++;
+            }
+        }
+    }
+
+    // To prevent memory leaks, dispose of the CancellationTokenSource when the VM is no longer needed
+    public void Dispose()
+    {
+        _cts.Cancel();
+        _cts.Dispose();
+    }
 }
