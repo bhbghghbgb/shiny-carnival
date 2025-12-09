be68d435be436de62887d5d2c3387e1726e8afa1
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-12-09 19:28:39 +0800

    Tạo API Interfaces  Refit

---
+++ UnoApp3/UnoApp3/Services/Api/IProductApi.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using UnoApp3.Models.Common;
+using UnoApp3.Models.Product;
+
+namespace UnoApp3.Services.Api;
+
+public interface IProductApi
+{
+    [Get("/admin/products")]
+    Task<Models.Common.ApiResponse<PagedList<ProductListDto>>> GetProducts([Query] ProductSearchRequest request);
+
+    [Get("/admin/products/{id}")]
+    Task<Models.Common.ApiResponse<ProductResponseDto>> GetProduct(int id);
+}
