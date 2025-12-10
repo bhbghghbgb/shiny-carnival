455bdbd29030baa1aff6c239a9aa1c66e9a527b0
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-12-10 10:25:38 +0800

    Tạo Services Layer

---
+++ UnoApp3/UnoApp3/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using UnoApp3.Models.Order;
+using UnoApp3.Services.Api;
+
+namespace UnoApp3.Services;
+
+public class OrderService
+{
+    private readonly IOrderApi _orderApi;
+
+    public OrderService(IOrderApi orderApi)
+    {
+        _orderApi = orderApi;
+    }
+
+    public async Task<OrderDetailsDto> CreateOrderAsync(CreateOrderRequest request)
+    {
+        var response = await _orderApi.CreateOrder(request);
+        return response?.Data;
+    }
+}
---
+++ UnoApp3/UnoApp3/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using UnoApp3.Models.Common;
+using UnoApp3.Models.Product;
+using UnoApp3.Services.Api;
+
+namespace UnoApp3.Services;
+
+public class ProductService
+{
+    private readonly IProductApi _productApi;
+
+    public ProductService(IProductApi productApi)
+    {
+        _productApi = productApi;
+    }
+
+    public async Task<PagedList<ProductListDto>> SearchProductsAsync(ProductSearchRequest request)
+    {
+        var response = await _productApi.GetProducts(request);
+        return response?.Data;
+    }
+
+    public async Task<ProductResponseDto> GetProductAsync(int id)
+    {
+        var response = await _productApi.GetProduct(id);
+        return response?.Data;
+    }
+
+    public string GetProductImageUrl(int productId, string baseUrl)
+    {
+        return $"{baseUrl}/images/{productId}";
+    }
+}
