0a7e249a9f27059256cd4b0f561725635bb4067d
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-12-11 09:36:36 +0800

    Tạo Views 1/?

---
+++ UnoApp3/UnoApp3/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using UnoApp3.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnoApp3.Views;
+
+public sealed partial class LoginPage : Page
+{
+    public LoginViewModel ViewModel => (LoginViewModel)DataContext;
+    
+    public LoginPage()
+    {
+        this.InitializeComponent();
+        DataContext = App.Services.GetRequiredService<LoginViewModel>();
+    }
+}
