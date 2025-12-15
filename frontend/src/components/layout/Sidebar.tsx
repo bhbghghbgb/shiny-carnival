e72ea4b0d51ed845121b7f4e497ce9f43f04bcc1
Nguyen Thanh Hung <nthungdev.140503@gmail.com>
2025-12-15 08:24:25 +0700

    feat(Sidebar): add user avatar and username display for authenticated users
    - Integrated Avatar and Typography components from Ant Design to show user information.
    - Displayed user avatar and username in the sidebar when the user is authenticated and the sidebar is not collapsed.

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Menu } from 'antd'
+import { Avatar, Menu, Typography } from 'antd'
 import {
   AppstoreOutlined,
   ContainerOutlined,
@@ -11,6 +11,7 @@ import {
 import type { MenuProps } from 'antd';
 import { Link } from "@tanstack/react-router";
 import { ENDPOINTS } from '../../app/routes/type/routes.endpoint';
+import { useAuthStore } from '../../features/auth/store/authStore';
 
 type MenuItem = Required<MenuProps>['items'][number]
 
@@ -41,11 +42,28 @@ interface SidebarProps {
 }
 
 const Sidebar = ({ collapsed }: SidebarProps) => {
+  const user = useAuthStore((state) => state.user);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+
   return (
     <div>
       <div className="p-4 text-2xl font-bold text-white text-center">
         Logo
       </div>
+      {isAuthenticated && !collapsed && (
+        <div className="px-4 py-3 mb-4 bg-gray-800 rounded-md mx-2 text-white flex items-center space-x-3">
+          <Avatar 
+            size="large"
+            style={{ backgroundColor: '#1890ff', color: '#fff', fontWeight: 'bold' }}
+          >
+            {user?.username ? user.username.charAt(0).toUpperCase() : "O"}
+          </Avatar>
+          <Typography.Text ellipsis style={{ color: 'white', fontWeight: '500', marginLeft: "10px" }}>
+            {user?.username || "No Name"}
+          </Typography.Text>
+        </div>
+      )}
       <Menu
         defaultSelectedKeys={['1']}
         mode="inline"
